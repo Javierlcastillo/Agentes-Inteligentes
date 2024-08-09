76905dab648b52ce36fd92c69569d94b40e9808c
Javierlcastillo <114962069+Javierlcastillo@users.noreply.github.com>
2024-08-09 01:51:14 +0000

    Agrego segunda clase y codigo de prueba esperando que este bien. NO LO HE CORRIDO

--- Clases en Python.py
+++ Clases en Python.py
@@ -1,6 +1,6 @@
 class Estudiante:
     def __init__(self, nombre, edad, cursos_inscritos):
-        self.nombreombre = nombre
+        self.nombre = nombre
         self.edad = edad
         self.cursos_inscritos = [] 
 
@@ -15,3 +15,62 @@ class Estudiante:
     def ver_cursos_inscritos(self):
         return self.cursos_inscritos
     
+class Curso:
+    def __init__(self, nombre_curso, horario):
+        self.nombre_curso = nombre_curso
+        self.horario = horario
+        self.estudiantes_inscritos = []
+    def inscribir_estudiante(self, estudiante):
+        if estudiante not in self.estudiantes_inscritos:
+            self.estudiantes_inscritos.append(estudiante)
+    def baja_de_estudiante(self, estudiante):
+        if estudiante in self.estudiantes_inscritos:
+            self.estudiantes_inscritos.remove(estudiante)
+    def ver_estudiantes_inscritos(self):
+        return self.estudiantes_inscritos
+
+# Creación de instancias
+estudiante1 = Estudiante("Jesús", 20)
+estudiante2 = Estudiante("María", 22)
+estudiante3 = Estudiante("José", 19)
+estudiante4 = Estudiante("Rosario", 24)
+estudiante5 = Estudiante("Pablo", 20)
+estudiante6 = Estudiante("Magdalena", 21)
+
+curso1 = Curso("Matemáticas", "Lunes y Miércoles")
+curso2 = Curso("Historia", "Martes y Jueves")
+
+# Inscripción a cursos
+curso1.inscribir_estudiante(estudiante1)
+curso1.inscribir_estudiante(estudiante2)
+curso1.inscribir_estudiante(estudiante5)
+curso1.inscribir_estudiante(estudiante6)
+curso1.inscribir_estudiante(estudiante2)
+curso2.inscribir_estudiante(estudiante1)
+curso2.inscribir_estudiante(estudiante3)
+curso2.inscribir_estudiante(estudiante4)
+curso2.inscribir_estudiante(estudiante6)
+print(">> DESPUÉS DE LAS INSCRIPCIONES <<")
+estudiante1.ver_cursos_inscritos()
+estudiante2.ver_cursos_inscritos()
+estudiante3.ver_cursos_inscritos()
+estudiante4.ver_cursos_inscritos()
+estudiante5.ver_cursos_inscritos()
+estudiante6.ver_cursos_inscritos()
+curso1.ver_estudiantes_inscritos()
+curso2.ver_estudiantes_inscritos()
+
+# Baja de cursos
+estudiante1.darse_de_baja(curso1)
+estudiante2.darse_de_baja(curso1)
+estudiante3.darse_de_baja(curso1)
+estudiante6.darse_de_baja(curso2)
+print("\n>> DESPUÉS DE LAS BAJAS <<")
+estudiante1.ver_cursos_inscritos()
+estudiante2.ver_cursos_inscritos()
+estudiante3.ver_cursos_inscritos()
+estudiante4.ver_cursos_inscritos()
+estudiante5.ver_cursos_inscritos()
+estudiante6.ver_cursos_inscritos()
+curso1.ver_estudiantes_inscritos()
+curso2.ver_estudiantes_inscritos()
