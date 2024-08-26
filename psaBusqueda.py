0e2551d5b4a49c5bea98704d51bbca0904b99749
Javierlcastillo <jazer2228@gmail.com>
2024-08-25 19:06:03 -0600

    Manana tengo asesoria con el profesor por que creo que me estoy mamando

--- psaBusqueda.py
+++ psaBusqueda.py
@@ -15,17 +15,63 @@ class Ubicacion:
         self.x = equis
         self.y = ye
 """
-class Tablero(SearchProblem):
-    def __init__(self, llenas, vacias):
-        super().__init__(llenas)
-        self.goal_state = vacias
 
-    def actions(self, Ubicacion):
+# class Vacias(object):
+#     def __init__(self, locx, locy):
+#         self.x = locx
+#         self.y = locy
+
+# class Llenas(object):
+#     def __init__(self, locx, locy):
+#         self.x = locx
+#         self.y = locy
+
+class Tablero(SearchProblem):
+    def __init__(self, ubicacion_inicial, llenas, vacias):
+        self.ubicacion_inicial = ubicacion_inicial
+        SearchProblem.__init__(self.ubicacion_inicial)
+        final = (len(llenas) == 0) 
+        self.goal_state = final
+
+    def actions(self, state):
+        Ubicacion, llenas, vacias = state #Tengo que hacer esta variable global?
         actions = []
         posibles_movimientos = [(2,1), (2,-1), (-2,1), (-2,-1), (1,2), (1,-2), (-1,2), (-1,-2)]
         for dx,dy in posibles_movimientos:
             if (0 <= Ubicacion.x + dx < 4 and 0 <= Ubicacion.y + dy< 4):
-                actions.append[(Ubicacion.setx, Ubicacion.sety)]
+                new_x = Ubicacion.setx(dx)
+                new_y = Ubicacion.sety(dy)
+                actions.append(new_x, new_y)
+                for i in range(len(llenas)):
+                    if llenas[i] == (new_x, new_y):
+                        vacias.append((new_x, new_y))
+                        llenas.remove(llenas[i])
+                        break
         return actions
 
+    # def result(self, state, action):
+    #     Ubicacion, llenas, vacias = state
+    
+    def is_goal(self, state):
+        return state == self.goal_state
 
+    def cost(self):
+        return 1
+    
+    def heuristic(self, vacias):
+        return len(vacias)
+    
+def display(result):
+    if result is not None:
+        for i, (action, state) in enumerate(result.path()):
+            if action == None:
+                print('Configuración inicial')
+            elif i == len(result.path()) - 1:
+                print(i,'- Después de moverse a', 'Ir a '+action)
+                print('¡Meta lograda con costo = ', result.cost,'!')
+            else:
+                print(i,'- Después de moverse a', 'Ir a '+action)
+
+            print('  ', state)
+    else:
+        print('Mala configuración del problema')
