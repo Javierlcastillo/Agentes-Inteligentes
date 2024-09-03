a3df6cd24595d6f29de9ce15f5ffe916e5c0389f
Javierlcastillo <jazer2228@gmail.com>
2024-09-03 16:57:10 -0600

    Comienzo de la actividad 4

---
+++ act4.py
@@ -0,0 +1,12 @@
+import math
+
+class OptimizacionGrafica(object):
+    def __init__(self, valoresx, valoresy):
+        self.a
+        self.b
+        self.c
+        self.d
+
+        self.x = valoresx
+        self.y = valoresy 
+        self.ecuacion = (math.cos(self.a*self.x) + self.b * x + - c * x^2 + d)
